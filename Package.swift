8822dd8167733f073e4bc40ff3286907842e829d
huyvuq <vuh@go.stockton.edu>
2022-12-30 21:00:25 -0500

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,25 @@
+
+
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "VegaScroll",
+    platforms: [.iOS(.v9)],
+    products: [
+        .library(name: "VegaScroll", targets: ["VegaScroll"]),
+    ],
+    targets: [
+        .target(
+            name: "VegaScroll",
+            dependencies: [],
+            path: "Sources"
+        )
+//       ,
+//         .testTarget(
+//             name: "VegaScrollTests",
+//             dependencies: ["VegaScroll"],
+//             path: "VegaScrollTest"),
+    ],
+    swiftLanguageVersions: [.v5]
+)
